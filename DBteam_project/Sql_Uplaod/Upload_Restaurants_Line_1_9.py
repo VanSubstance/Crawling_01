684749a1a9401725dea07d1611342476515f907a
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2020-06-14 16:32:01 +0900

    .

--- DBteam_project/Sql_Uplaod/Upload_Restaurants_Line_1_9.py
+++ DBteam_project/Sql_Uplaod/Upload_Restaurants_Line_1_9.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-station = ['Line_07', 'Line_08']
+station = ['Data_03', 'Data_04']
 
 dir_excel = (os.path.dirname(os.getcwd()) + "\\Restaurant").replace("\\", "/")
 
